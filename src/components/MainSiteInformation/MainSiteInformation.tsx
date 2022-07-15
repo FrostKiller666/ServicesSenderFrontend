29e4e42d8400b1d20e519249f1dd3752a3839a78
FrostKiller <bialywilk50@gmail.com>
2022-07-15 22:50:52 +0200

    fex margin in main site

--- src/components/MainSiteInformation/MainSiteInformation.tsx
+++ src/components/MainSiteInformation/MainSiteInformation.tsx
@@ -29,7 +29,7 @@ const MainSiteInformation = () => {
                             <Image  src={'https://miro.medium.com/max/576/1*7FgTI6QxrfaZdA2DVg3w0g.png'}/>
                         </Col>
                         <Col>
-                            <h3 className={'text-center fw-bold'}>Jak zacząć</h3>
+                            <h3 className={'text-center fw-bold pb-4'}>Jak zacząć</h3>
                             <p className={'text-break fs-6'}>
                                 To bradzo proste, aby korzystać z usług musisz w pierwszej kolejności stworzyć bezpłatne konto by mieć dostęp do treści w naszym serwisie. Oczywiście nie musisz się obawiać podawać swojego hasła z racji tego, że jest bezpiecznie przechowywane w naszej bazie danych jako wygenerowany ciąg znaków, dodatkowo zalogowany użtkownik jest chroniony specjalnym tokenem identyfikacyjnym przechowywanym po stronie servera oraz odpowiednio zabezpieczony. Jako użytkonik masz możliwość zmiany swojego hasła oraz nicku, wykorzstując do tego panel w prawym górnym ekranie widooczny jako Twoja nmazwa użytkonika podana przy procesie rejestracji.
                             </p>
@@ -40,7 +40,7 @@ const MainSiteInformation = () => {
                 <Container className={styles.card}>
                     <Row>
                         <Col>
-                            <h3 className={'text-center fw-bold'}>Dlaczego Warto</h3>
+                            <h3 className={'text-center fw-bold pb-4'}>Dlaczego Warto</h3>
                             <p className={'text-break fs-6'}>
                                 To bardzo oczywiste, na pewno borykałeś się już wielokrotnie z uciążliwością pisania mozolnych meli aby wykonywać zamówienie. Właśnie tutaj przychodmiy z pomocą i automatyzujemy to za Ciebie. Dzięki naszemu sposobowi wysłania danych masz w pełni zwalidowane informacje, które chceszwysłać, a co za tym idzie? Nie jesteś w stanie popełnić żadnego błędu i możesz cieszyć się z wolnego czasu, który zaoszczędziłeś. Nie martw się nasza usługa pozwala wysyłać kilka zgłoszeń jednocześnie. Zatem nie marnuj czasu i wyślij swoje pierwsze zamówienie.
                             </p>
